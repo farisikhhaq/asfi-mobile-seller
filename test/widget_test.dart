105ad8b7c77afe4275d0224e93b037aba956e037
farisikhhaq <farisikhlasulhaq@yahoo.co.id>
2026-05-26 01:07:11 +0700

    refactor: rename app widget in tests and add devtools configuration file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:asfi_mobile_seller/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AsfiSellerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
